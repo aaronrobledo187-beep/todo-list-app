15a13831777ab9d1cddb8ef3bfcfa6f1d159ce10
aaron <aaronrobledo187@gmail.com>
2025-10-28 17:40:33 +0000

    finished

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Script started");
+console.log("Script started"); 
+
+function addtask(event){
+    // Get user input form
+    let textbook = document.getElementById("task-input");
+    let task = texbox.value;
+    console.log(task);
+    // Create a list item
+    
+    let todoList = document.getElementById("todo-list");
+    // create list-item div
+    let listDiv = document.createElement("div");
+     listDiv.id = "task" + todoList.childElementCount;
+     listDiv.classList.add("list-item");
+     
+
+    // create checkbox
+      let checkbox = document.createElement("input");
+      checkbox.type ="checkbox";
+      checkbox.id = "checkbox" + todoList.childElementCount;
+
+    // create label
+    let label = document.createElement("label");
+    label.id = "label" + todoList.childElementCount;
+    label.innerText = task;
+    
+    // apend checkbox to list=item div
+    listDiv.appendChild(checkbox);
+
+    // ppend label to div 
+    listDiv.appendChild(label);
+    // append list-item div to the list
+    todolist.appendChild(listDiv); 
+}    
